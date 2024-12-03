b16ed07da339dbd2a696cf3c8b3237d3f5912a57
Tobbe Gyllebring <tobbe@drunkcod.com>
2024-12-03 09:42:03 +0100

    Prettier config. OmitIndex. asswerOwn API aligned.

--- argis.spec.ts
+++ argis.spec.ts
@@ -2,73 +2,82 @@ import { describe, expect, it, test } from '@jest/globals';
 import { ArgumentError, argNotNil, isNil, isNotNil, assertOwn, assertNotNil, intOrUndefined, hasOwn } from './index.js';
 
 describe('isNil', () => {
-    test('null', () => expect(isNil(null)).toBe(true));
-    test('undefined', () => expect(isNil(void 0)).toBe(true));
-    test('{}', () => expect(isNil({})).toBe(false));
-    test('x.value is nil', () => expect(isNil({ value: null }, 'value')).toBe(true));
-    test('x.value is not nil', () => expect(isNil({ value: 42 }, 'value')).toBe(false));
-    test('x.value when x is nil', () => expect(isNil(null as unknown as ({ value: string }), 'value')).toBe(true));
+	test('null', () => expect(isNil(null)).toBe(true));
+	test('undefined', () => expect(isNil(void 0)).toBe(true));
+	test('{}', () => expect(isNil({})).toBe(false));
+	test('x.value is nil', () => expect(isNil({ value: null }, 'value')).toBe(true));
+	test('x.value is not nil', () => expect(isNil({ value: 42 }, 'value')).toBe(false));
+	test('x.value when x is nil', () => expect(isNil(null as unknown as { value: string }, 'value')).toBe(true));
 });
 
 describe('isNotNil', () => {
-    test('x', () => expect(isNotNil({})).toBe(true));
-    test('x.value', () => expect(isNotNil({ value: 42}, 'value')).toBe(true));
-    test('x.null', () => expect(isNotNil({ null: null}, 'null')).toBe(false));
+	test('x', () => expect(isNotNil({})).toBe(true));
+	test('x.value', () => expect(isNotNil({ value: 42 }, 'value')).toBe(true));
+	test('x.null', () => expect(isNotNil({ null: null }, 'null')).toBe(false));
 });
 
 describe('argNotNil', () => {
-    it('raise ArgumentError on null', () => {
-        expect(() => argNotNil({ x: null as unknown as string}, 'x')).toThrow(ArgumentError)
-    });
-    it('rases ArgumentError on undefined', () => {
-        expect(() => argNotNil({ x: undefined as unknown as string}, 'x')).toThrow(ArgumentError)
-    });
+	it('raise ArgumentError on null', () => {
+		expect(() => argNotNil({ x: null as unknown as string }, 'x')).toThrow(ArgumentError);
+	});
+	it('rases ArgumentError on undefined', () => {
+		expect(() => argNotNil({ x: undefined as unknown as string }, 'x')).toThrow(ArgumentError);
+	});
 });
 
 describe('hasOwn', () => {
-    test('exists', () => {
-        expect(hasOwn({ answer: 42}, 'answer')).toBeTruthy();
-    });
-    test('missing', () => {
-        expect(hasOwn({ answer: 42}, 'message')).toBeFalsy();
-    });
-    test('with type check', () => {
-        const it = { answer: 42 };
-        expect(hasOwn(it, 'answer', (x: unknown) => typeof x === 'number')).toBeTruthy();
-        //it.answer is Number.
-    });
-    test('with type check failing', () => {
-        const it = { answer: 42 };
-        expect(hasOwn(it, 'answer', (x: unknown) => typeof x === 'object')).toBeFalsy();
-    })
+	test('exists', () => {
+		expect(hasOwn({ answer: 42 }, 'answer')).toBeTruthy();
+	});
+	test('missing', () => {
+		expect(hasOwn({ answer: 42 }, 'message')).toBeFalsy();
+	});
+	test('with type check', () => {
+		const it = { answer: 42 };
+		expect(hasOwn(it, 'answer', (x: unknown) => typeof x === 'number')).toBeTruthy();
+		//it.answer is Number.
+	});
+	test('with type check failing', () => {
+		const it = { answer: 42 };
+		expect(hasOwn(it, 'answer', (x: unknown) => typeof x === 'object')).toBeFalsy();
+	});
 });
 
 describe('assertOwn', () => {
-    it('raise ArgumentError if missing', () => {
-        expect(() => assertOwn({}, 'missing')).toThrow(ArgumentError);
-    });
-    it('raise ArgumentError if not own', () => {
-        expect(() => assertOwn({}, 'toString')).toThrow(ArgumentError);
-    });
-    it('narrows type', () => {
-        const x = { message: 'hello world'} as object;
-        assertOwn(x, 'message');
-        expect(x.message).toEqual('hello world');
-    });
+	it('raise ArgumentError if missing', () => {
+		expect(() => assertOwn({}, 'missing')).toThrow(ArgumentError);
+	});
+	it('raise ArgumentError if not own', () => {
+		expect(() => assertOwn({}, 'toString')).toThrow(ArgumentError);
+	});
+	it('narrows type', () => {
+		const x = { message: 'hello world' } as object;
+		assertOwn(x, 'message');
+		expect(x.message).toEqual('hello world');
+	});
+	test('with type check', () => {
+		const it = { answer: 42 as unknown };
+		assertOwn(it, 'answer', (x: unknown) => typeof x === 'number');
+		expect(it.answer).toBe(42);
+	});
+	test('with type check failing', () => {
+		const it = { answer: 42 };
+		expect(() => assertOwn(it, 'answer', (x: unknown) => typeof x === 'object')).toThrow(ArgumentError);
+	});
 });
 
 describe('assertNotNil', () => {
-    it('raises ArgumentError on null', () => {
-        expect(() => assertNotNil(null as unknown as object)).toThrow();
-    });
-    it('raises ArgumentError on undefined', () => {
-        expect(() => assertNotNil(undefined)).toThrow();
-    });
-})
+	it('raises ArgumentError on null', () => {
+		expect(() => assertNotNil(null as unknown as object)).toThrow();
+	});
+	it('raises ArgumentError on undefined', () => {
+		expect(() => assertNotNil(undefined)).toThrow();
+	});
+});
 
 describe('intOrUndefined', () => {
-    test("valid string", () => expect(intOrUndefined("42")).toEqual(42));
-    test("empty string", () => expect(intOrUndefined("")).toBeUndefined());
-    test("null string", () => expect(intOrUndefined(null)).toBeUndefined());
-    test("undefined string", () => expect(intOrUndefined(null)).toBeUndefined());
-})
+	test('valid string', () => expect(intOrUndefined('42')).toEqual(42));
+	test('empty string', () => expect(intOrUndefined('')).toBeUndefined());
+	test('null string', () => expect(intOrUndefined(null)).toBeUndefined());
+	test('undefined string', () => expect(intOrUndefined(null)).toBeUndefined());
+});
--- index.ts
+++ index.ts
@@ -1,66 +1,73 @@
 type NoNullMembers<T> = { [P in keyof T]: NonNullable<T[P]> };
 
 export type WithRequired<T, K extends keyof T> = Required<NoNullMembers<Pick<T, K>>> & Omit<T, K>;
-export type WithKey<K extends PropertyKey,V = unknown> = { [p in K]: V }
+
+export type WithKey<K extends PropertyKey, V = unknown> = { [p in K]: V };
+
+export type OmitIndex<T> = {
+	[K in keyof T as string extends K ? never : number extends K ? never : symbol extends K ? never : K]: T[K];
+};
 
 export class ArgumentError extends Error {
-    constructor(message: string) {
-        super(message);
-    }
+	constructor(message: string) {
+		super(message);
+	}
 
-    static null(message: string): never {
-        const e = new ArgumentError(message);
-        e.name = 'ArgumentNullError';
-        throw e;
-    }
+	static null(message: string): never {
+		const e = new ArgumentError(message);
+		e.name = 'ArgumentNullError';
+		throw e;
+	}
 
-    static missing({ key }: { key: PropertyKey}): never {
-        const e = new ArgumentError(`Missing property ${String(key)}.`);
-        e.name = 'MissingPropertyError';
-        throw e;
-    }
+	static missing({ key }: { key: PropertyKey }): never {
+		const e = new ArgumentError(`Missing property ${String(key)}.`);
+		e.name = 'MissingPropertyError';
+		throw e;
+	}
 }
 
 export function isNil(x: any): x is null | undefined;
 export function isNil<T extends object>(x: T | null, key: keyof T): x is T & WithKey<typeof key, null | undefined>;
 export function isNil(x: any, key?: any): boolean {
-    return x == null || (key !== undefined && x[key] == null);
+	return x == null || (key !== undefined && x[key] == null);
 }
 
 export function isNotNil<T>(x?: T): x is T;
-export function isNotNil<T, K extends keyof T>(x: T, key: K): x is T & { [P in K]-?: NonNullable<T[K]> } 
+export function isNotNil<T, K extends keyof T>(x: T, key: K): x is T & { [P in K]-?: NonNullable<T[K]> };
 export function isNotNil<T, K extends keyof T>(x?: T, key?: K) {
 	return !isNil(x) && (key === undefined || !isNil(x[key]));
 }
 
 export function assertNotNil<T>(x: T | null): asserts x is T {
-    if(x == null) ArgumentError.null('Unexpected null');
+	if (x == null) ArgumentError.null('Unexpected null');
 }
 
 export function argNotNil<T, K extends keyof T>(x: T, key: K): asserts x is T & { [P in K]-?: NonNullable<T[K]> } {
-    isNotNil(x, key) || ArgumentError.null(`'${String(key)}' was null or undefined`);
+	isNotNil(x, key) || ArgumentError.null(`'${String(key)}' was null or undefined`);
 }
 
 function _hasOwn<T extends object, K extends PropertyKey>(x: T, key: K): x is T & WithKey<K> {
-    return Object.hasOwn(x, key)
+	return Object.hasOwn(x, key);
 }
 
 export function hasOwn<T extends object, K extends PropertyKey>(x: T, key: K): x is T & WithKey<K>;
 export function hasOwn<T extends object, K extends PropertyKey, V>(x: T, key: K, ofType: (found: unknown) => found is V): x is T & WithKey<K, V>;
-export function hasOwn<T extends object, K extends PropertyKey, V = unknown>(x: T, key: K, ofType?: (found: unknown) => found is V): x is T & WithKey<K, V>{
-    return _hasOwn(x, key) && (ofType == undefined || ofType(x[key]))
+export function hasOwn<T extends object, K extends PropertyKey, V = unknown>(x: T, key: K, ofType?: (found: unknown) => found is V): x is T & WithKey<K, V> {
+	return _hasOwn(x, key) && (ofType == undefined || ofType(x[key]));
 }
 
-export function assertOwn<T extends object, K extends PropertyKey>(x: T, key: K): asserts x is T & WithKey<K> {
-    hasOwn(x, key) || ArgumentError.missing({ key });
+export function assertOwn<T extends object, K extends PropertyKey>(x: T, key: K): asserts x is T & WithKey<K>;
+export function assertOwn<T extends object, K extends PropertyKey, V>(x: T, key: K, ofType: (found: unknown) => found is V): asserts x is T & WithKey<K, V>;
+export function assertOwn<T extends object, K extends PropertyKey, V = unknown>(x: T, key: K, ofType?: (found: unknown) => found is V): asserts x is T & WithKey<K, V> {
+	(ofType ? hasOwn(x, key, ofType) : hasOwn(x, key)) || ArgumentError.missing({ key });
 }
 
 export function nullIfEmpty<T extends { length: number }>(x: T | null) {
-     return x == null || x.length == 0 ? null : x;
+	return x == null || x.length == 0 ? null : x;
 }
 
-export function intOrUndefined(x?: string | null) : number | undefined {
-    if(isNil(x)) return undefined;
-    const v = Number.parseInt(x);
-    return Number.isNaN(v) ? undefined : v;
+export function intOrUndefined(x?: string | null): number | undefined {
+	if (isNil(x)) return undefined;
+	const v = Number.parseInt(x);
+	return Number.isNaN(v) ? undefined : v;
 }
