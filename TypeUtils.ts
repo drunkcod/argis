79597755c1cf4bb8fbe18e21e76b608663ad9f87
Tobbe Gyllebring <tobbe@drunkcod.com>
2026-03-14 09:33:37 +0100

    Initial cycle detetcion.

--- TypeUtils.ts
+++ TypeUtils.ts
@@ -1,10 +1,11 @@
 export type Pretty<T> = { [P in keyof T]: T[P] } & {};
 
 const SPECIAL_TAG: unique symbol = Symbol('');
-type SpecialTag<T> = { readonly [SPECIAL_TAG]: T };
+type SpecialTag<T, Data = never> = { readonly [SPECIAL_TAG]: T; data: Data };
 type TagAny = SpecialTag<'any'>;
 type TagOptional = SpecialTag<'?'>;
 type TagUnknown = SpecialTag<'unknown'>;
+export type TagCycle<T> = SpecialTag<'cycle', T>;
 
 export type IsAny<T> = 0 extends 1 & T ? true : false;
 export type IsUnknown<T> = unknown extends T ? (IsAny<T> extends true ? false : true) : false;
@@ -20,6 +21,12 @@ type TagSpecial<T> = {
 		IsUnknown<T[P]> extends true ? TagUnknown :
 		T[P]);
 };
+
+// prettier-ignore
+export type TagCycles<T, Visited = never> = 
+	[T] extends [Visited] ? TagCycle<T> :
+	T extends object ? { [P in keyof T]: TagCycles<T[P], Visited | T> } :
+	T;
 
 type IfTagged<T, X extends SpecialTag<any>, True, False> = [Extract<T, X>] extends [never] ? False : True;
 
